e6d3669166927cae427a61afe23f81f38e125812
Anton Bauer <anton.bauer2@generic.de>
2024-06-02 12:56:46 +0200

    fix namespaces

--- wapi/UseCases/ServiceCollectionExtensions.cs
+++ wapi/UseCases/ServiceCollectionExtensions.cs
@@ -1,4 +1,4 @@
-using CounterMotinor.Infrastructure.DataAccess;
+using Template.Infrastructure.DataAccess;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
