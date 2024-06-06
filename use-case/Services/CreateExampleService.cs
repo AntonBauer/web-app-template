99f61ba957816d937c8c3b36dc3623b3543b0395
Anton Bauer <anton.bauer2@generic.de>
2024-06-06 09:58:10 +0200

    fix example service

--- use-case/Services/CreateExampleService.cs
+++ use-case/Services/CreateExampleService.cs
@@ -7,16 +7,9 @@ namespace Template.UseCases.CreateExample.Services;
 
 internal sealed class CreateExampleService(ExampleContext context)
 {
-  public async Task<Result<CreatedExampleDto>> Create(string name, CancellationToken cancellationToken)
-  {
+  public async Task<Result<CreatedExampleDto>> Create(string name, CancellationToken cancellationToken) =>
     ExampleEntity.Create(name)
                  .Tap(example => context.Examples.Add(example))
                  .Tap(example => context.SaveChangesAsync(cancellationToken))
                  .Map(example => example.ToCreatedExampleDto());
-
-    context.Examples.Add(exampleResult);
-    await context.SaveChangesAsync(cancellationToken);
-
-    return exampleResult.ToCreatedExampleDto();
-  }
 }
